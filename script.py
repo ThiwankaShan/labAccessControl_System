f5c8060254cd97aaced81c0213ea9182d7624b24
ThiwankaShan <shane.thiwanka@gmail.com>
2020-10-17 21:29:44 +0530

    detect faces froman array of known faces

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 import face_recognition as fr
 import cv2
+import os 
 
-known_image = fr.load_image_file("known_faces/face.jpg")
-known_encoding = fr.face_encodings(known_image)[0]
+directory="known_faces/"
+known_faces=[]
+known_names=[]
+for fileName in os.listdir(directory):
+    known_image = fr.load_image_file(f"known_faces/{fileName}")
+    known_encoding = fr.face_encodings(known_image)[0]
+    known_faces.append(known_encoding)
+    known_names.append(fileName[:-4])
+
 
 cap = cv2.VideoCapture("video/video.mp4")
 
@@ -10,13 +18,16 @@ while (cap.isOpened()):
     ret, frame = cap.read()
     
     face_locations=fr.face_locations(frame)
-    unknown_encoding = fr.face_encodings(frame,face_locations)
+    faces_encoding = fr.face_encodings(frame,face_locations)
 
-    for unknown_face in unknown_encoding:
-        result=fr.compare_faces([known_encoding],unknown_face)
-
+    for face in faces_encoding:
+        result=fr.compare_faces(known_faces,face)
+        
         if True in result:
-            print('keira knightley')
+            index=result.index(True)
+            print(known_names[index])
+
     cv2.imshow('video frame',frame)
     if cv2.waitKey(1)==ord('q'):
         break
+
